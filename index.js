31f636fdc2fe35c9157d4930cdca92f8ad73391d
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-02-28 02:27:30 -0300

    Change ids to classes

--- index.js
+++ index.js
@@ -65,13 +65,13 @@ document.getElementById("fullscreen-button").addEventListener("click", () => {
   }
 
   if (isFullScreen) {
-    document.getElementById("enter-fullscreen").style.display = "";
-    document.getElementById("exit-fullscreen").style.display = "none";
+    document.querySelector(".enter-fullscreen").style.display = "";
+    document.querySelector(".exit-fullscreen").style.display = "none";
     document.getElementById("fullscreen-button").style.opacity = 0.2;
     document.getElementById("github-link").style.opacity = 0.1;
   } else {
-    document.getElementById("enter-fullscreen").style.display = "none";
-    document.getElementById("exit-fullscreen").style.display = "";
+    document.querySelector(".enter-fullscreen").style.display = "none";
+    document.querySelector(".exit-fullscreen").style.display = "";
     document.getElementById("fullscreen-button").style.opacity = 0.1;
     document.getElementById("github-link").style.opacity = 0;
   }
